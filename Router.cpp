95c1173e15894a2a8b43b4671b1e11d3aded7470
Benjamin Tamási <h@lfto.me>
2016-12-15 16:15:01 +0100

    sample 2 files in directory

--- Router.cpp
+++ Router.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <cstring>
+#include <zconf.h>
 #include "Router.h"
+
+fuse_operations Router::ops = {0};
+
+void Router::setup() {
+    ops.init = init;
+    ops.destroy = destroy;
+    ops.opendir = opendir;
+    ops.readdir = readdir;
+    ops.getattr = getattr;
+}
 
 void *Router::init(struct fuse_conn_info *conn) {
     std::cout << "FUSE version: " << conn->proto_major << "." << conn->proto_major << std::endl;
@@ -7,17 +19,44 @@ void *Router::init(struct fuse_conn_info *conn) {
 }
 
 void Router::destroy(void *private_data) {
-    std::cout << "FUSE unloading..." << std::endl;
 }
 
-int Router::readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+int Router::getattr(const char *path, struct stat *st)
+{
+    st->st_uid = getuid();
+    st->st_gid = getgid();
+    st->st_atime = time(NULL);
+    st->st_mtime = time(NULL);
+
+    if (strcmp(path, "/") == 0) {
+        st->st_mode = S_IFDIR | 0755;
+        st->st_nlink = 2;
+    } else {
+        st->st_mode = S_IFREG | 0644;
+        st->st_nlink = 1;
+        st->st_size = 1024;
+    }
+
     return 0;
 }
 
-fuse_operations Router::ops = {0};
+int Router::opendir(const char *path, struct fuse_file_info *fi) {
+    if (strcmp(path, "/") != 0) {
+        return -1;
+    }
+    fi->fh = 1;
+    return 0;
+}
 
-void Router::setup() {
-    ops.readdir = readdir;
-    ops.init = init;
-    ops.destroy = destroy;
+int Router::readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+    filler( buf, ".", NULL, 0 );
+    filler( buf, "..", NULL, 0 );
+
+    if (strcmp(path, "/") == 0)
+    {
+        filler(buf, "file1", NULL, 0 );
+        filler(buf, "file2", NULL, 0 );
+    }
+
+    return 0;
 }
--- Router.h
+++ Router.h
@@ -8,7 +8,9 @@ public:
     static struct fuse_operations ops;
     static void* init(struct fuse_conn_info *conn);
     static void destroy(void* private_data);
+    static int getattr(const char* path, struct stat *statbuf);
     static int readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi);
+    static int opendir(const char* path, struct fuse_file_info* fi);
 };
 
 
