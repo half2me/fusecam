f7589d5cf3e9badeab43161df26e2d5eac921ba1
Benjamin Tamási <h@lfto.me>
2016-12-15 12:06:18 +0100

    cmake working bitches...

--- main.cpp
+++ main.cpp
@@ -5,7 +5,5 @@ int main(int argc, char *argv[]) {
     struct fuse_operations ops;
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
     fuse_main(args.argc, args.argv, &ops, NULL);
-
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
