9ba7139aab0725d4d94c6d2fb66b075c6c54482a
Tamási Benjamin <halftome@yahoo.com>
2016-12-17 12:47:51 +0100

    Travis

--- Camera.cpp
+++ Camera.cpp
@@ -2,10 +2,10 @@
 
 Camera::Camera() {
     // debug generate streams and io
-    setStream("str1", *new Stream());
-    setStream("str2", *new Stream());
-    setIo("io1", *new Io());
-    setIo("io2", *new Io());
+    //setStream("str1", *new Stream());
+    //setStream("str2", *new Stream());
+    //setIo("io1", *new Io());
+    //setIo("io2", *new Io());
 }
 
 Camera::~Camera() {
